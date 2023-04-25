ddfc57b10bd4106ab3a85d92d1de5e5c1b6a9d58
riolly <alberto.riolly@gmail.com>
2023-04-25 12:01:46 +0700

    fix: localhost determination during expo local dev

--- apps/expo/src/utils/api.tsx
+++ apps/expo/src/utils/api.tsx
@@ -27,7 +27,10 @@ const getBaseUrl = () => {
    * **NOTE**: This is only for development. In production, you'll want to set the
    * baseUrl to your production API URL.
    */
-  const localhost = Constants.manifest?.debuggerHost?.split(":")[0];
+  const debuggerHost =
+    Constants.manifest?.debuggerHost ??
+    Constants.manifest2?.extra?.expoGo?.debuggerHost;
+  const localhost = debuggerHost?.split(":")[0];
   if (!localhost) {
     const apiUrl = Constants.expoConfig?.extra?.apiUrl as string;
     if (apiUrl) return apiUrl;
