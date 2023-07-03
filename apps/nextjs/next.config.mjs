5afc61bededc52e5c6671eb3cf8c6eac495b7ee2
riolly <alberto.riolly@gmail.com>
2023-07-03 14:04:49 +0700

    feat: t3-env separation

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -1,8 +1,4 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
- * This is especially useful for Docker builds and Linting.
- */
-// !process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"));
+import "./src/env.mjs";
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -10,8 +6,8 @@ const config = {
   /** Enables hot reloading for local packages without a build step */
   transpilePackages: ["@acme/api", "@acme/db"],
   /** We already do linting and typechecking as separate tasks in CI */
-  eslint: { ignoreDuringBuilds: !!process.env.CI },
-  typescript: { ignoreBuildErrors: !!process.env.CI },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 };
 
 export default config;
